3745917f413743d517a98d4532ab9a66b3a5f051
duzumaki <thegoduzumaki@icloud.com>
2023-12-03 21:18:56 +0000

    day 3 part 2

---
+++ day3_part1.py
@@ -0,0 +1,109 @@
+from collections import defaultdict
+
+grid = []
+
+# create grid
+with open("test.txt", "r") as f:
+    for line in f:
+        row = []
+        for line_character in line:
+            if line_character == "\n":
+                continue
+            row.append(line_character)
+        grid.append(row)
+
+rows = len(grid)
+cols = len(grid[0])
+
+
+# dfs
+def find_number(row: int, col: int, number_collector: list, search_diag: bool = False):
+    if row < 0 or col < 0 or row >= rows or col >= cols:
+        return
+
+    if grid[row][col] == ".":
+        return
+
+    if grid[row][col] == "seen":
+        return
+
+    if grid[row][col].isdigit() and grid[row][col] != "seen":
+        number_collector += (grid[row][col], row, col)
+        grid[row][col] = "seen"
+
+    down = find_number(row + 1, col, number_collector)
+    up = find_number(row - 1, col, number_collector)
+    right = find_number(row, col + 1, number_collector)
+    left = find_number(row, col - 1, number_collector)
+
+    if search_diag:
+        diag_upper_right = find_number(
+            row - 1,
+            col + 1,
+            number_collector,
+        )
+        diag_upper_left = find_number(row - 1, col - 1, number_collector)
+        diag_lower_left = find_number(row + 1, col - 1, number_collector)
+        diag_lower_right = find_number(row + 1, col + 1, number_collector)
+
+
+def grid_pass(rows: int, cols: int, search_diag: False, number_collector: list):
+    global output
+    numbers = []
+    for row in range(rows):
+        for col in range(cols):
+            cell = grid[row][col]
+
+            if not cell.isalnum() and cell != ".":
+                find_number(
+                    row,
+                    col,
+                    number_collector=number_collector,
+                    search_diag=search_diag,
+                )
+                if number_collector:
+                    # print(number_collector)
+                    numbers.append(number_collector)
+                    print(number_collector)
+                    number_collector = []
+
+    output = []
+    grouped_numbers = defaultdict(list)
+
+    for number in numbers:
+        for i in range(0, len(number), 3):
+            digit, row, col = number[i], number[i + 1], number[i + 2]
+
+            grouped_numbers[row].append((col, digit))
+
+    numbers_in_correct_order = []
+    # sort by column position
+    for row in grouped_numbers:
+        current_numbers = []
+        prev_col = None
+        for col_digit in sorted(grouped_numbers[row], key=lambda x: x[0]):
+            col, digit = col_digit
+
+            if prev_col and (abs(prev_col - col) > 1):
+                numbers_in_correct_order.append(int("".join(current_numbers)))
+                current_numbers = []
+
+            prev_col = col
+            current_numbers.append(digit)
+        numbers_in_correct_order.append(int("".join(current_numbers)))
+
+    return numbers_in_correct_order
+
+
+# pass 1
+# collect the numbers then mark the grid to mark all up, down, right, left touching
+# symbol numbers as seen
+pass1 = grid_pass(rows, cols, search_diag=False, number_collector=[])
+
+
+# pass 2
+# collect the numbers then mark the grid to mark all up, down, right, left touching
+# symbol numbers as seen
+pass2 = grid_pass(rows, cols, search_diag=True, number_collector=[])
+
+print(sum(pass1) + sum(pass2))
--- day_2_part2.c
+++ day_2_part2.c
@@ -1,47 +1,21 @@
 #include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-int CUBED_SUM = 0;
 
 int main() {
     FILE *file = fopen("test.txt", "r");
-    int token_count;
 
     int number;
+
     char buffer[128];
 
-    int cubed_sum = 0;
+    int game_no;
+    char buffer2[128];
 
 
-    while (fscanf(file, "%s %d:", buffer, &number) == 2) {
-        int game_no = number;
-
-        int red = 0;
-        int green = 0;
-        int blue = 0;
-
-        while (fscanf(file, "%d %s", &number, buffer) == 2) {
-            // check the first character only so you don't need to 
-            // bother parsing the rest of the string. e.g "blue,", "red;", etc
-            if (buffer[0] == 'r' && number > red) {
-                red = number;
-            } 
-            else if (buffer[0] == 'g' && number > green) {
-                green = number;
-            }
-            else if(buffer[0] == 'b' && number > blue) {
-                blue = number;
-            }
+        while (fscanf(file, "%s %d: %d %s", buffer2, &game_no, &number, buffer) == 2) {
+            printf("%s\n", buffer);
         }
 
-        int product = red * green * blue;
-        cubed_sum += product;
-    }
 
     fclose(file);
-    printf("%d", cubed_sum);
     return 0;
 }
-
-
